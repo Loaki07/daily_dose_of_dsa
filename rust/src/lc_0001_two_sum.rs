0774aa68ff2f4776e69b7dd65a1a15ff2235a9de
loaki07 <rustyloaki@gmail.com>
2024-01-02 23:50:06 +0530

    add: alternate soln to two_sum

--- rust/src/lc_0001_two_sum.rs
+++ rust/src/lc_0001_two_sum.rs
@@ -1,19 +1,31 @@
 use std::collections::HashMap;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut checked_elements: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
-        for (i, v) in nums.iter().enumerate() {
-            let needed_v = target - v;
+    let mut checked_elements: HashMap<i32, i32> =
+        HashMap::with_capacity(nums.len());
+    for (i, v) in nums.iter().enumerate() {
+        let needed_v = target - v;
 
-            match checked_elements.get(&v) {
-                Some(&val) => return vec![i as i32, val],
-                None => {
-                    checked_elements.insert(needed_v, i as i32);
-                }
+        match checked_elements.get(&v) {
+            Some(&val) => return vec![i as i32, val],
+            None => {
+                checked_elements.insert(needed_v, i as i32);
             }
         }
-        vec![]
     }
+    vec![]
+}
+
+pub fn _two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for i in 0..nums.len() {
+        for j in (i + 1)..nums.len() {
+            if nums[i] + nums[j] == target {
+                return vec![i as i32, j as i32];
+            }
+        }
+    }
+    vec![]
+}
 
 #[cfg(test)]
 mod test {
@@ -21,16 +33,21 @@ mod test {
 
     #[test]
     fn ex1() {
-        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![1, 0])
+        let result = two_sum(vec![2, 7, 11, 15], 9);
+        assert!(
+            result == vec![0, 1] || result == vec![1, 0]
+        );
     }
 
     #[test]
     fn ex2() {
-        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![2, 1])
+        let res = two_sum(vec![3, 2, 4], 6);
+        assert!(res == vec![1, 2] || res == vec![2, 1]);
     }
 
     #[test]
     fn ex3() {
-        assert_eq!(two_sum(vec![3, 3], 6), vec![1, 0]);
+        let res = two_sum(vec![3, 3], 6);
+        assert!(res == vec![0, 1] || res == vec![1, 0]);
     }
 }
