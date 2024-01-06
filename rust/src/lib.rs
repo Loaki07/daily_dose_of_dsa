6dfc63388cd7011fa948a64625dd5c645d856827
loaki07 <rustyloaki@gmail.com>
2024-01-07 01:37:46 +0530

    modify: lib.rs in sequential order of problem numbers

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,9 +4,6 @@ pub use utils::*;
 pub mod lc_0001_two_sum;
 pub use lc_0001_two_sum::*;
 
-pub mod lc_0509_fibonacci_number;
-pub use lc_0509_fibonacci_number::*;
-
 pub mod lc_0020_valid_parentheses;
 pub use lc_0020_valid_parentheses::*;
 
@@ -18,3 +15,6 @@ pub use lc_0121_best_time_to_buy_and_sell_stock::*;
 
 pub mod lc_0125_valid_palindrome;
 pub use lc_0125_valid_palindrome::*;
+
+pub mod lc_0509_fibonacci_number;
+pub use lc_0509_fibonacci_number::*;
