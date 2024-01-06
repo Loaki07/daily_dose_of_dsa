4e6a11516190e7e691b63186966310d56ff6a9c8
loaki07 <rustyloaki@gmail.com>
2024-01-07 01:44:29 +0530

    add: soln to prob242 valid anagram

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,5 +16,8 @@ pub use lc_0121_best_time_to_buy_and_sell_stock::*;
 pub mod lc_0125_valid_palindrome;
 pub use lc_0125_valid_palindrome::*;
 
+pub mod lc_0242_valid_anagram;
+pub use lc_0242_valid_anagram::*;
+
 pub mod lc_0509_fibonacci_number;
 pub use lc_0509_fibonacci_number::*;
