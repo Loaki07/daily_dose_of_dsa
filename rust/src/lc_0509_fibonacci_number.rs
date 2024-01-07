7ae3a86e32077f498293b025faabd7bd627c684e
loaki07 <rustyloaki@gmail.com>
2024-01-08 01:52:03 +0530

    modify: prob 509 fibonacci edge case if n is 0 or 1

--- rust/src/lc_0509_fibonacci_number.rs
+++ rust/src/lc_0509_fibonacci_number.rs
@@ -8,6 +8,13 @@ pub fn fib(n: i32) -> i32 {
 }
 
 pub fn fib_iter(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+
     let mut a = 0;
     let mut b = 1;
     let mut res = 1;
