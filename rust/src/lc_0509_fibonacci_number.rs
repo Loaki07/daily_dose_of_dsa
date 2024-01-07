4d3f474f932e39b0195a25fb5a294f342eca5cbf
loaki07 <rustyloaki@gmail.com>
2024-01-08 01:48:26 +0530

    add: soln to fibonacci prob_no 509, iter

--- rust/src/lc_0509_fibonacci_number.rs
+++ rust/src/lc_0509_fibonacci_number.rs
@@ -7,6 +7,20 @@ pub fn fib(n: i32) -> i32 {
     }
 }
 
+pub fn fib_iter(n: i32) -> i32 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut res = 1;
+
+    for _ in 1..n {
+        res = a + b;
+        a = b;
+        b = res;
+    }
+
+    res
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -14,15 +28,18 @@ mod test {
     #[test]
     fn test_1() {
         assert_eq!(fib(2), 1);
+        assert_eq!(fib_iter(2), 1);
     }
 
     #[test]
     fn test_2() {
         assert_eq!(fib(3), 2);
+        assert_eq!(fib_iter(3), 2);
     }
 
     #[test]
     fn test_3() {
         assert_eq!(fib(4), 3);
+        assert_eq!(fib_iter(4), 3);
     }
 }
