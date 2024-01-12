bab7f846368f679bb665d6ed4bd0fdb429866a68
loaki07 <rustyloaki@gmail.com>
2024-01-12 14:57:27 +0530

    add: util doubly_linked_list

---
+++ rust/src/utils/doubly_linked_list.rs
@@ -0,0 +1,177 @@
+// Immutable outside, but can mutate interior.
+use std::cell::RefCell;
+// Reference counting pointer
+// using weak reference here, to avoid cyclic
+// condition where a node has a reference to
+// itself (a circle loop where child is pointing
+// to parent, and parent is pointing to child) https://doc.rust-lang.org/std/rc/struct.Weak.html
+// we can drop the weak in such condtiions
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+pub struct Node<T: Copy> {
+    pub value: T,
+    pub next: Option<Rc<RefCell<Node<T>>>>,
+    pub prev: Option<Weak<RefCell<Node<T>>>>,
+}
+
+impl<T: Copy> Node<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            value,
+            next: None,
+            prev: None,
+        }
+    }
+}
+
+impl<T: Copy> From<Node<T>>
+    for Option<Rc<RefCell<Node<T>>>>
+{
+    fn from(node: Node<T>) -> Self {
+        Some(Rc::new(RefCell::new(node)))
+    }
+}
+
+type NodePtr<T> = Rc<RefCell<Node<T>>>;
+
+#[derive(Debug)]
+pub struct List<T: Copy> {
+    head: Option<NodePtr<T>>,
+    tail: Option<NodePtr<T>>,
+}
+
+impl<T: Copy> List<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, value: T) {
+        let mut node = Node::new(value);
+
+        match &mut self.head.take() {
+            None => {
+                self.head = node.into();
+                self.tail = self.head.clone();
+            }
+            Some(current_head) => {
+                node.next = Some(current_head.clone());
+                self.head = node.into();
+                if let Some(h) = &self.head {
+                    current_head.borrow_mut().prev =
+                        Some(Rc::downgrade(&h));
+                }
+            }
+        }
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        let mut node = Node::new(value);
+
+        match &mut self.tail.take() {
+            Some(current_tail) => {
+                node.prev =
+                    Some(Rc::downgrade(&current_tail));
+                self.tail = node.into();
+                current_tail.borrow_mut().next =
+                    self.tail.clone();
+            }
+            None => {
+                self.head = node.into();
+                self.tail = self.head.clone();
+            }
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        match &mut self.head.take() {
+            None => None,
+            Some(head) => {
+                let mut head = head.borrow_mut();
+                let next = head.next.take();
+
+                match next {
+                    None => {
+                        self.tail.take();
+                    }
+                    Some(next) => {
+                        next.borrow_mut().prev = None;
+                        self.head = Some(next);
+                    }
+                }
+
+                Some(head.value)
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        match &mut self.tail.take() {
+            Some(tail) => {
+                let mut tail = tail.borrow_mut();
+                let prev = tail.prev.take();
+
+                match prev {
+                    Some(prev) => {
+                        let prev = prev.upgrade();
+                        if let Some(prev) = prev {
+                            prev.borrow_mut().next = None;
+                            self.tail = Some(prev);
+                        }
+                    }
+                    None => {
+                        self.head.take();
+                    }
+                };
+                Some(tail.value)
+            }
+            None => None,
+        }
+    }
+}
+
+impl<T: Copy> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_back() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn doubly_linked_list_construction_back() {
+        let mut list = List::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        // dbg!(&list);
+
+        assert_eq!(list.pop_back(), Some(4));
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn doubly_linked_list_construction_front() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        list.push_front(4);
+        dbg!(&list);
+
+        assert_eq!(list.pop_front(), Some(4));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+}
--- rust/src/utils/mod.rs
+++ rust/src/utils/mod.rs
@@ -24,3 +24,6 @@ pub use heap_sort_min_heap::*;
 
 pub mod singly_linked_list;
 pub use singly_linked_list::*;
+
+pub mod doubly_linked_list;
+pub use doubly_linked_list::*;
