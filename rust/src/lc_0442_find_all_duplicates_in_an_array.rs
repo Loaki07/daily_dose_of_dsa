18f8b93f266f5935f0f8c3acd564aabe57aa3081
loaki07 <rustyloaki@gmail.com>
2024-01-14 00:21:16 +0530

    add: using sets, soln to prob 0442 find all duplicates in an array

--- rust/src/lc_0442_find_all_duplicates_in_an_array.rs
+++ rust/src/lc_0442_find_all_duplicates_in_an_array.rs
@@ -1,3 +1,7 @@
+use std::collections::{HashMap, HashSet};
+
+use rayon::result;
+
 // O(n^2)
 pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
     let mut result: Vec<i32> = Vec::new();
@@ -17,6 +21,25 @@ pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
     result
 }
 
+// for an unsorted array
+pub fn find_duplicates_set(nums: Vec<i32>) -> Vec<i32> {
+    let mut hm = HashMap::<i32, i32>::new();
+    let mut result = HashSet::<i32>::new();
+    if nums.len() == 0 {
+        return vec![];
+    }
+
+    for i in nums {
+        let v = hm.entry(i).or_insert(0);
+        *v += 1;
+        if *v > 1 {
+            result.insert(i);
+        }
+    }
+
+    result.into_iter().collect::<Vec<i32>>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,6 +47,7 @@ mod tests {
     #[test]
     fn works_to_find_duplicates_with_empty() {
         assert_eq!(find_duplicates(vec![]), vec![]);
+        assert_eq!(find_duplicates_set(vec![]), vec![]);
     }
 
     #[test]
@@ -32,15 +56,26 @@ mod tests {
             find_duplicates(vec![4, 3, 2, 7, 8, 2, 3, 1]),
             vec![2, 3]
         );
+        assert_eq!(
+            find_duplicates_set(vec![
+                4, 3, 2, 7, 8, 2, 3, 1
+            ]),
+            vec![2, 3]
+        );
     }
 
     #[test]
     fn ex2() {
         assert_eq!(find_duplicates(vec![1, 1, 2]), vec![1]);
+        assert_eq!(
+            find_duplicates_set(vec![1, 1, 2]),
+            vec![1]
+        );
     }
 
     #[test]
     fn ex3() {
         assert_eq!(find_duplicates(vec![1]), vec![]);
+        assert_eq!(find_duplicates_set(vec![1]), vec![]);
     }
 }
