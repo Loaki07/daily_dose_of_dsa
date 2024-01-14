268f6b232f92f9cfeb3aef169cc0c5135b31ff03
loaki07 <rustyloaki@gmail.com>
2024-01-15 04:20:06 +0530

    add: soln to prob 543 diameter of binary tree, dfs

--- rust/src/lc_trees/mod.rs
+++ rust/src/lc_trees/mod.rs
@@ -6,3 +6,6 @@ pub use lc_0226_invert_binary_tree::*;
 
 pub mod lc_0235_lowest_common_ancestor_of_a_binary_search_tree;
 pub use lc_0235_lowest_common_ancestor_of_a_binary_search_tree::*;
+
+pub mod lc_0543_diameter_of_binary_tree;
+pub use lc_0543_diameter_of_binary_tree::*;
