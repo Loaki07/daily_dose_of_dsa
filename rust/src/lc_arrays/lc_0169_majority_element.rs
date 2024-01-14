a01e89120b2310fd9b687d1ac5eba548d8fa4bf5
loaki07 <rustyloaki@gmail.com>
2024-01-15 03:01:31 +0530

    add: soln to prob 169 majority element

---
+++ rust/src/lc_arrays/lc_0169_majority_element.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+
+        let mut hm = HashMap::new();
+
+        for i in nums {
+            *hm.entry(i).or_insert(0) += 1;
+        }
+
+        let mut vec =
+            hm.into_iter().collect::<Vec<(i32, i32)>>();
+        vec.sort_by(|a, b| b.1.cmp(&a.1));
+
+        vec.get(0).unwrap().0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert_eq!(
+            Solution::majority_element(vec![3, 2, 3]),
+            3
+        );
+    }
+
+    #[test]
+    fn ex2() {
+        assert_eq!(
+            Solution::majority_element(vec![
+                2, 2, 1, 1, 1, 2, 2
+            ]),
+            2
+        )
+    }
+}
--- rust/src/lc_arrays/mod.rs
+++ rust/src/lc_arrays/mod.rs
@@ -7,6 +7,9 @@ pub use lc_0075_sort_colors::*;
 pub mod lc_0121_best_time_to_buy_and_sell_stock;
 pub use lc_0121_best_time_to_buy_and_sell_stock::*;
 
+pub mod lc_0169_majority_element;
+pub use lc_0169_majority_element::*;
+
 pub mod lc_0217_contains_duplicate;
 pub use lc_0217_contains_duplicate::*;
 
