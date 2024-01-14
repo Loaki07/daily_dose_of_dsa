5c6ac33aa7ecab31e94ef6fa9014d42fb94cbc4d
loaki07 <rustyloaki@gmail.com>
2024-01-15 01:36:16 +0530

    add: soln to prob 383 ransom note, hashtable

---
+++ rust/src/lc_strings/lc_0383_ransom_note.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+struct Solution;
+
+impl Solution {
+    // The code first creates a HashMap (hm) to store
+    // the frequency of each character in magazine.
+
+    // It iterates over each character (ch) in
+    // magazine. For each character, it uses the
+    // entry API of HashMap to fetch or insert a
+    // default value of 0. It then increments the
+    // count of this character
+    // (*hm.entry(ch).or_insert(0) += 1;).
+    // After this loop, hm contains the count of every
+    // character available in the magazine.
+    //     The code then iterates over each character
+    // in the ransom_note:
+
+    // For each character in ransom_note, it checks
+    // the frequency of that character in hm.
+    // If the character is not found in the magazine
+    // (i.e., its count in hm is 0), the function
+    // returns false, indicating that the ransom note
+    // cannot be constructed. If the character is
+    // found, it decrements the count in hm and
+    // continues the process.
+    pub fn can_construct(
+        ransom_note: String,
+        magazine: String,
+    ) -> bool {
+        let mut hm = HashMap::new();
+
+        for ch in magazine.chars() {
+            *hm.entry(ch).or_insert(0) += 1;
+        }
+
+        for ch in ransom_note.chars() {
+            let e = hm.entry(ch).or_insert(0);
+            if *e == 0 {
+                return false;
+            }
+            *e -= 1;
+        }
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert!(!Solution::can_construct(
+            "a".to_string(),
+            "b".to_string()
+        ));
+    }
+
+    #[test]
+    fn ex2() {
+        assert!(!Solution::can_construct(
+            "aa".to_string(),
+            "ab".to_string()
+        ));
+    }
+
+    #[test]
+    fn ex3() {
+        assert!(Solution::can_construct(
+            "aa".to_string(),
+            "aab".to_string()
+        ));
+    }
+}
--- rust/src/lc_strings/mod.rs
+++ rust/src/lc_strings/mod.rs
@@ -3,3 +3,6 @@ pub use lc_0125_valid_palindrome::*;
 
 pub mod lc_0242_valid_anagram;
 pub use lc_0242_valid_anagram::*;
+
+pub mod lc_0383_ransom_note;
+pub use lc_0383_ransom_note::*;
