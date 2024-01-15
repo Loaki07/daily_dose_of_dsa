ee672705dd5a6d869ccf3f310381f820c3bd1e0d
loaki07 <rustyloaki@gmail.com>
2024-01-15 12:48:27 +0530

    add: soln to prob 542 01 matrix, faster runtime and solved edge case

--- rust/src/lc_graphs/lc_0542_01_matrix.rs
+++ rust/src/lc_graphs/lc_0542_01_matrix.rs
@@ -4,6 +4,83 @@ impl Solution {
     pub fn update_matrix(
         mat: Vec<Vec<i32>>,
     ) -> Vec<Vec<i32>> {
+        use std::collections::HashSet;
+
+        let width = mat[0].len(); // n -> coloumns
+        let height = mat.len(); // m -> rows
+        let mut output = vec![vec![0; width]; height];
+        let max = (width + height) as i32;
+
+        let mut hs = HashSet::new();
+
+        for row in 0..height {
+            for col in 0..width {
+                if mat[row][col] == 0 {
+                    output[row][col] = 0;
+                } else {
+                    // setting the output vec to max
+                    // possible value for the moment
+                    output[row][col] = max;
+                    hs.insert((row, col));
+                }
+            }
+        }
+
+        while !hs.is_empty() {
+            let entries =
+                hs.drain().collect::<Vec<(usize, usize)>>();
+
+            for (row, col) in entries {
+                let up = if row == 0 {
+                    None
+                } else {
+                    Some((row - 1, col))
+                };
+                let down = if row + 1 == height {
+                    None
+                } else {
+                    Some((row + 1, col))
+                };
+                let left = if col == 0 {
+                    None
+                } else {
+                    Some((row, col - 1))
+                };
+                let right = if col + 1 == width {
+                    None
+                } else {
+                    Some((row, col + 1))
+                };
+
+                let mut min = max;
+                for dir in [up, down, left, right]
+                    .iter()
+                    .filter_map(|&x| x)
+                {
+                    min = min.min(output[dir.0][dir.1]);
+                }
+                min += 1;
+
+                if min < output[row][col] {
+                    output[row][col] = min;
+                    for dir in [up, down, left, right]
+                        .iter()
+                        .filter_map(|&x| x)
+                    {
+                        hs.insert(dir);
+                    }
+                }
+            }
+        }
+
+        output
+    }
+
+    // has an edge case index out of bounds
+    // eg: 5 x 1 matrix
+    pub fn _update_matrix(
+        mat: Vec<Vec<i32>>,
+    ) -> Vec<Vec<i32>> {
         let width = mat[0].len(); // n -> coloumns
         let height = mat.len(); // m -> rows
         let mut output = vec![vec![0; width]; height];
@@ -93,6 +170,26 @@ mod test {
                 vec![0, 0, 0],
                 vec![0, 1, 0],
                 vec![1, 2, 1]
+            ]
+        );
+    }
+
+    #[test]
+    fn ex3() {
+        assert_eq!(
+            Solution::update_matrix(vec![
+                vec![0],
+                vec![0],
+                vec![0],
+                vec![0],
+                vec![0],
+            ]),
+            vec![
+                vec![0],
+                vec![0],
+                vec![0],
+                vec![0],
+                vec![0],
             ]
         );
     }
