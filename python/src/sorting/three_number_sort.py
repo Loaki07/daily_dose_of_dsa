f6c3b485c112f809a9afb03dac84fbc52ef4280d
loaki07 <rustyloaki@gmail.com>
2024-02-08 10:42:17 +0530

    add: python three_number_sort, 2 pass method

--- python/src/sorting/three_number_sort.py
+++ python/src/sorting/three_number_sort.py
@@ -3,7 +3,38 @@ from typing import Any, List
 
 # O(n) time
 # O(1) space
-def three_number_sort(
+# 2 pass method
+def three_number_sort_2(
+    array: List[Any], order: List[Any]
+) -> List[Any]:
+    first_value = order[0]
+    third_value = order[2]
+
+    first_idx = 0
+    for idx in range(len(array)):
+        if array[idx] == first_value:
+            array[first_idx], array[idx] = (
+                array[idx],
+                array[first_idx],
+            )
+            first_idx += 1
+
+    third_idx = len(array) - 1
+    for idx in range(len(array) - 1, -1, -1):
+        if array[idx] == third_value:
+            array[third_idx], array[idx] = (
+                array[idx],
+                array[third_idx],
+            )
+            third_idx -= 1
+
+    return array
+
+
+# O(n) time
+# O(1) space
+# bucket sort
+def three_number_sort_1(
     array: List[Any], order: List[Any]
 ) -> List[Any]:
     value_counts = [0, 0, 0]
@@ -25,8 +56,15 @@ def three_number_sort(
 
 
 # Test
-def test_three_number_sort():
-    result = three_number_sort(
+def test_three_number_sort_1():
+    result = three_number_sort_1(
+        [1, 0, 0, -1, -1, 0, 1, 1], [0, 1, -1]
+    )
+    assert result == [0, 0, 0, 1, 1, 1, -1, -1]
+
+
+def test_three_number_sort_2():
+    result = three_number_sort_2(
         [1, 0, 0, -1, -1, 0, 1, 1], [0, 1, -1]
     )
     assert result == [0, 0, 0, 1, 1, 1, -1, -1]
