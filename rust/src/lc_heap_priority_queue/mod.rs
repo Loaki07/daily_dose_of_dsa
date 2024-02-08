c09f28e7e493a81741476c54a8b28eb515b8cc74
loaki07 <rustyloaki@gmail.com>
2024-02-08 14:55:47 +0530

    refactor: prob 215 to heap

--- rust/src/lc_heap_priority_queue/mod.rs
+++ rust/src/lc_heap_priority_queue/mod.rs
@@ -1,2 +1,5 @@
+pub mod lc_0215_kth_largest_element_in_an_array;
+pub use lc_0215_kth_largest_element_in_an_array::*;
+
 pub mod lc_0973_k_closest_points_to_origin;
 pub use lc_0973_k_closest_points_to_origin::*;
--- rust/src/lc_searching/mod.rs
+++ rust/src/lc_searching/mod.rs
@@ -4,9 +4,6 @@ pub use lc_0033_search_in_rotated_sorted_array::*;
 pub mod lc_0035_search_insert_position;
 pub use lc_0035_search_insert_position::*;
 
-pub mod lc_0215_kth_largest_element_in_an_array;
-pub use lc_0215_kth_largest_element_in_an_array::*;
-
 pub mod lc_0278_first_bad_version;
 pub use lc_0278_first_bad_version::*;
 
