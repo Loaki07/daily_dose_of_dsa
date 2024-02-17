85503effba04261893fbe8875258517b33469be8
loaki07 <rustyloaki@gmail.com>
2024-02-17 12:53:57 +0530

    add: python common_character, all unique characters

--- python/src/strings/common_characters.py
+++ python/src/strings/common_characters.py
@@ -1,10 +1,42 @@
-from typing import List
+from typing import List, Set
+
+
+# Method 2
+# O(n * m) time | O(m) space
+def common_characters(strings: List[str]) -> List[str]:
+    smallest_string = get_smallest_string(strings)
+    potential_common_characters = set(smallest_string)
+
+    for string in strings:
+        remove_non_existant_characters(
+            string, potential_common_characters
+        )
+    return list(potential_common_characters)
+
+
+def get_smallest_string(strings: List[str]) -> str:
+    smallest_string = strings[0]
+    for string in strings:
+        if len(string) < len(smallest_string):
+            smallest_string = string
+
+    return smallest_string
+
+
+def remove_non_existant_characters(
+    string: str, potential_common_characters
+):
+    unique_string_characters = set(string)
+
+    for character in list(potential_common_characters):
+        if character not in unique_string_characters:
+            potential_common_characters.remove(character)
 
 
 # Method 1: using all the unique strings
 # from the list of strings
 # O(n * m) time | O(c) space
-def common_characters(strings: List[str]) -> List[str]:
+def common_characters_1(strings: List[str]) -> List[str]:
     character_counts = {}
     for string in strings:
         unique_string_characters = set(string)
