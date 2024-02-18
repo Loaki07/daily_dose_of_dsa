83e46a19bf77fa902b4eb9f37f005569b661c4c3
loaki07 <rustyloaki@gmail.com>
2024-02-18 20:11:57 +0530

    add: rust prob 121 best_time_to_buy_and_sell_stock, two_pointers

--- rust/src/lc_sliding_window/lc_0121_best_time_to_buy_and_sell_stock.rs
+++ rust/src/lc_sliding_window/lc_0121_best_time_to_buy_and_sell_stock.rs
@@ -1,4 +1,27 @@
+// basic two pointers for the sliding window
 pub fn max_profit(prices: Vec<i32>) -> i32 {
+    if prices.len() < 2 {
+        return 0;
+    }
+
+    let mut l = 0;
+    let mut r = 1;
+    let mut max_profit = 0;
+
+    while r < prices.len() {
+        if prices[l] < prices[r] {
+            let profit = prices[r] - prices[l];
+            max_profit = profit.max(max_profit);
+        } else {
+            l = r;
+        }
+        r += 1;
+    }
+
+    max_profit
+}
+
+pub fn _max_profit(prices: Vec<i32>) -> i32 {
     if prices.len() < 2 {
         return 0;
     }
