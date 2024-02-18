6dd3133e4f5565525f86489740b788b711b9e6b7
loaki07 <rustyloaki@gmail.com>
2024-02-18 16:44:37 +0530

    add: practice_rust_1 prob 0347 top_k_frequent_elements, hashmap & binary heap

---
+++ practice_rust_1/src/lc_arrays_hashing/lc_0347_top_k_frequent_elements.rs
@@ -0,0 +1,53 @@
+pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    use std::collections::{BinaryHeap, HashMap};
+
+    let mut hm: HashMap<i32, usize> = HashMap::new();
+
+    for num in nums {
+        hm.entry(num)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+
+    hm.into_iter()
+        .map(|(num, count)| (count, num))
+        .collect::<BinaryHeap<(usize, i32)>>()
+        .into_sorted_vec()
+        .into_iter()
+        .rev()
+        .take(k as usize)
+        .map(|(_, num)| num)
+        .collect::<Vec<i32>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        let nums = vec![1, 1, 1, 2, 2, 3];
+        let k = 2;
+        let mut result = top_k_frequent(nums, k);
+        result.sort();
+        let mut expected_res = vec![1, 2];
+        expected_res.sort();
+        assert_eq!(result, expected_res);
+    }
+
+    // #[test]
+    // fn ex2() {
+    //     let nums = vec![1];
+    //     let k = 1;
+    //     let result = top_k_frequent(nums, k);
+    //     assert_eq!(result, vec![1]);
+    // }
+
+    // #[test]
+    // fn ex3() {
+    //     let nums = vec![-1, -1];
+    //     let k = 1;
+    //     let result = top_k_frequent(nums, k);
+    //     assert_eq!(result, vec![-1]);
+    // }
+}
--- practice_rust_1/src/lc_arrays_hashing/mod.rs
+++ practice_rust_1/src/lc_arrays_hashing/mod.rs
@@ -6,3 +6,6 @@ pub use lc_0049_group_anagrams::*;
 
 pub mod lc_0217_contains_duplicate;
 pub use lc_0217_contains_duplicate::*;
+
+pub mod lc_0347_top_k_frequent_elements;
+pub use lc_0347_top_k_frequent_elements::*;
