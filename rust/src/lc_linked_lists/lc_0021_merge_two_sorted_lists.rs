ad40e799e265cb5e7ff3de9f69adc2728a0453e5
loaki07 <rustyloaki@gmail.com>
2024-02-19 09:18:48 +0530

    add: rust prob 21 merge two sorted lists, recursive soln

--- rust/src/lc_linked_lists/lc_0021_merge_two_sorted_lists.rs
+++ rust/src/lc_linked_lists/lc_0021_merge_two_sorted_lists.rs
@@ -1,6 +1,37 @@
 use crate::ListNode;
 
+// recursive solution
 pub fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    match (list1, list2) {
+        (Some(list1), None) => Some(list1),
+        (None, Some(list2)) => Some(list2),
+        (None, None) => None,
+        (Some(l1), Some(l2)) => {
+            if l1.val < l2.val {
+                return Some(Box::new(ListNode {
+                    val: l1.val,
+                    next: merge_two_lists(
+                        l1.next,
+                        Some(l2),
+                    ),
+                }));
+            } else {
+                return Some(Box::new(ListNode {
+                    val: l2.val,
+                    next: merge_two_lists(
+                        Some(l1),
+                        l2.next,
+                    ),
+                }));
+            }
+        }
+    }
+}
+
+pub fn _merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
@@ -51,7 +82,7 @@ pub fn merge_two_lists(
     result
 }
 
-pub fn _merge_two_lists(
+pub fn _merge_two_lists_1(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
