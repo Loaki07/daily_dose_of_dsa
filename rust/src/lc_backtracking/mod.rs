a8c03e063b1a64c9707476d6d8179b4a50ea63da
loaki07 <rustyloaki@gmail.com>
2024-03-26 00:03:34 +0530

    add: soln to prob 51 n_queens, backtracking

--- rust/src/lc_backtracking/mod.rs
+++ rust/src/lc_backtracking/mod.rs
@@ -10,6 +10,9 @@ pub use lc_0040_combination_sum_2::*;
 pub mod lc_0046_permutations;
 pub use lc_0046_permutations::*;
 
+pub mod lc_0051_n_queens;
+pub use lc_0051_n_queens::*;
+
 pub mod lc_0078_subsets;
 pub use lc_0078_subsets::*;
 
